d25a4b93aacdb2a96de07f41851c0e788fddc1bc
aasirvalji <aasir.valji@hotmail.com>
2021-02-17 16:53:36 -0500

    updated bot code and added Passive model

--- index.js
+++ index.js
@@ -50,114 +50,121 @@ var streamAuthorIds = {};
 var chatInChannel = {};
 
 client.on('message', async (message) => {
-  if (message.author.bot) return;
-  if (message.content.startsWith(PREFIX)) {
-    const [CMD_NAME, ...args] = message.content
-      .trim()
-      .substring(PREFIX.length)
-      .split(/\s+/);
+  try {
 
-    if (CMD_NAME === 'rank' && args[0] === '-a') {
-      if (!message.content.includes('r:') || !message.content.includes('p:') || !message.content.includes('i:')) 
-        return await message.channel.send(`Hmm you don't have everything for me to create a rank.`)
-
-      var rankStartingIndex = message.content.indexOf('r:');
-      var idStartingIndex = message.content.indexOf('i:');
-      var pointsStartingIndex = message.content.indexOf('p:');
-
-      var rankName = message.content.substring(rankStartingIndex, idStartingIndex - 1).replace(/\s+/g, ' ').trim();
-      var id = message.content.substring(idStartingIndex, pointsStartingIndex - 1).replace(/\s+/g, ' ').trim();
-      var points = message.content.substring(pointsStartingIndex, message.content.length).replace(/\s+/g, ' ').trim();
-
-      // remove prefixes
-      rankName = rankName.split('r: ')[1];
-      id = id.split('i: ')[1];
-      points = points.split('p: ')[1];
-
-      // r: rank name
-      // i: id of the role
-      // p: points required
-      console.log(`Rank name: ${rankName}, Id: ${id}, Points: ${points}`);
-
-      var rank = await Rank.findOne({ rankName: rankName });
-      if (rank) {
-        rank.pointsRequired = points;
-        rank.roleId = id;
-        await rank.save();
-        return await message.channel.send(`${rankName} has been updated.`);
-      }
-      
-      rank = await Rank.create({ rankName: rankName, roleId: id, pointsRequired: points });
-      return await message.channel.send(`${rankName} has been created.`);
-    }
-    else if (CMD_NAME === 'rank' && args[0] === '-r') {
-      var rawRankName = message.content.split(`${PREFIX}rank -r `)[1];
-      var rank = await Rank.findOne({ rankName: rawRankName });
-
-      if (!rank) return await message.channel.send(`That rank doesn't exist.`);
-
-      await rank.remove();
-      return await message.channel.send(`${rawRankName} has been removed.`);
-    } 
-  }
-  else {
-      if (message.attachments && message.attachments.values().next().value !== undefined) {
-        var attachementUrlArr = message.attachments.values().next().value.attachment.split('/');
-        var attachementType = attachementUrlArr[attachementUrlArr.length - 1];
-        attachementType = attachementType.substr(attachementType.length - 5);
-        if (!attachementType.includes('.png') && !attachementType.includes('.jpeg') && !attachementType.includes('.jpg')) return;
-
-        var user = await User.findOne({ discordId: message.author.id.toString() });
-        if (!user) user = await createDbUser(message.author.id.toString());
-
-        var questTemplate = await QuestTemplate.findOne({ type: 'image' });
-        var questInProgress = await QuestInProgress.findOne({ discordId: message.author.id.toString(), type: 'image' });
-        if (!questInProgress) questInProgress = await QuestInProgress.create({ discordId: message.author.id.toString(), type: 'image', counter: 1 });
-        else questInProgress.counter = questInProgress.counter + 1;
-        if (questInProgress.counter === questTemplate.successCounter) {
-          await questInProgress.remove();
-          user.completedQuests = [...user.completedQuests, questTemplate.id];
-          await user.save();
-          
-          const member = await client.guilds.cache.get(process.env.guildId).members.fetch(message);
-          if (!member) return console.log("Couln't find member for image quest on discord.");
-          await member.send(`You've completed the quest ${questTemplate.name}`);
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+      const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+  
+      if (CMD_NAME === 'rank' && args[0] === '-a') {
+        if (!message.content.includes('r:') || !message.content.includes('p:') || !message.content.includes('i:')) 
+          return await message.channel.send(`Hmm you don't have everything for me to create a rank.`)
+  
+        var rankStartingIndex = message.content.indexOf('r:');
+        var idStartingIndex = message.content.indexOf('i:');
+        var pointsStartingIndex = message.content.indexOf('p:');
+  
+        var rankName = message.content.substring(rankStartingIndex, idStartingIndex - 1).replace(/\s+/g, ' ').trim();
+        var id = message.content.substring(idStartingIndex, pointsStartingIndex - 1).replace(/\s+/g, ' ').trim();
+        var points = message.content.substring(pointsStartingIndex, message.content.length).replace(/\s+/g, ' ').trim();
+  
+        // remove prefixes
+        rankName = rankName.split('r: ')[1];
+        id = id.split('i: ')[1];
+        points = points.split('p: ')[1];
+  
+        // r: rank name
+        // i: id of the role
+        // p: points required
+        console.log(`Rank name: ${rankName}, Id: ${id}, Points: ${points}`);
+  
+        var rank = await Rank.findOne({ rankName: rankName });
+        if (rank) {
+          rank.pointsRequired = points;
+          rank.roleId = id;
+          await rank.save();
+          return await message.channel.send(`${rankName} has been updated.`);
         }
-        else await questInProgress.save()
+        
+        rank = await Rank.create({ rankName: rankName, roleId: id, pointsRequired: points });
+        return await message.channel.send(`${rankName} has been created.`);
       }
-
-      var isSpecific = await Achievement.findOne({ message: message.content, channelId: message.channel.id.toString() });
-      var quest = await QuestTemplate.findOne({ channelId: message.channel.id.toString(), type: "message-in-channel" });
-
-      if (isSpecific) {
-          // ACHIEVEMENT: User sends a specific message in a specific channel
+      else if (CMD_NAME === 'rank' && args[0] === '-r') {
+        var rawRankName = message.content.split(`${PREFIX}rank -r `)[1];
+        var rank = await Rank.findOne({ rankName: rawRankName });
+  
+        if (!rank) return await message.channel.send(`That rank doesn't exist.`);
+  
+        await rank.remove();
+        return await message.channel.send(`${rawRankName} has been removed.`);
+      } 
+    }
+    else {
+        if (message.attachments && message.attachments.values().next().value !== undefined) {
+          var attachementUrlArr = message.attachments.values().next().value.attachment.split('/');
+          var attachementType = attachementUrlArr[attachementUrlArr.length - 1];
+          attachementType = attachementType.substr(attachementType.length - 5);
+          if (!attachementType.includes('.png') && !attachementType.includes('.jpeg') && !attachementType.includes('.jpg')) return;
+  
           var user = await User.findOne({ discordId: message.author.id.toString() });
           if (!user) user = await createDbUser(message.author.id.toString());
-          user.completedAchievements = [...user.completedAchievements, isSpecific.id];
-          user.points = user.points + isSpecific.points;
-          await user.save();
-          const member = await client.guilds.cache.get(process.env.guildId).members.fetch(message.author.id.toString());
-          if (!member) return;
-          await member.send('Specific channel achievement unlocked.');
-      }
-
-      // chat in channel quest
-      if (message.channel.id.toString() === process.env.chatInChannelQuest) {
-        var quest = await QuestTemplate.findOne({ channelId: process.env.chatInChannelQuest, type: "message-in-channel" });
-        if (!quest) return console.log("Couldn't find message in channel quest.");
-        
-        var messageAtInMinutes = Math.round(message.createdTimestamp / parseInt(quest.message));
-        if (!chatInChannel[message.author.id.toString()]) chatInChannel[message.author.id.toString()] = [messageAtInSeconds];
-        else if (chatInChannel[message.author.id.toString()].isArray()){ // if quest not completed yet
-          var intervalExists = (chatInChannel[message.author.id.toString()]).find((t) => t === messageAtInMinutes);
-          if (!intervalExists) chatInChannel[message.author.id.toString()] = [...chatInChannel[message.author.id.toString()], messageAtInMinutes];
+  
+          var questTemplate = await QuestTemplate.findOne({ type: 'image' });
+          var questInProgress = await QuestInProgress.findOne({ discordId: message.author.id.toString(), type: 'image' });
+          if (!questInProgress) questInProgress = await QuestInProgress.create({ discordId: message.author.id.toString(), type: 'image', counter: 1 });
+          else questInProgress.counter = questInProgress.counter + 1;
+          if (questInProgress.counter === questTemplate.successCounter) {
+            await questInProgress.remove();
+            user.completedQuests = [...user.completedQuests, questTemplate.id];
+            await user.save();
+            
+            const member = await client.guilds.cache.get(process.env.guildId).members.fetch(message);
+            if (!member) return console.log("Couln't find member for image quest on discord.");
+            await member.send(`You've completed the quest ${questTemplate.name}`);
+          }
+          else await questInProgress.save()
         }
-      }
-
-      // add author id to array here for passive points (every min)
-      var authorExists = chatAuthorIds.find((a) => a === message.author.id.toString());
-      if (authorExists) return;
-      return chatAuthorIds.push(message.author.id.toString());
+  
+        var isSpecific = await Achievement.findOne({ message: message.content, channelId: message.channel.id.toString() });
+        var quest = await QuestTemplate.findOne({ channelId: message.channel.id.toString(), type: "message-in-channel" });
+  
+        if (isSpecific) {
+            // ACHIEVEMENT: User sends a specific message in a specific channel
+            var user = await User.findOne({ discordId: message.author.id.toString() });
+            if (!user) user = await createDbUser(message.author.id.toString());
+            user.completedAchievements = [...user.completedAchievements, isSpecific.id];
+            user.points = user.points + isSpecific.points;
+            user.coins = user.coins + isSpecific.points;
+            await user.save();
+            const member = await client.guilds.cache.get(process.env.guildId).members.fetch(message.author.id.toString());
+            if (!member) return;
+            await member.send('Specific channel achievement unlocked.');
+        }
+  
+        // chat in channel quest
+        if (message.channel.id.toString() === process.env.chatInChannelQuest) {
+          var quest = await QuestTemplate.findOne({ channelId: process.env.chatInChannelQuest, type: "message-in-channel" });
+          if (!quest) return console.log("Couldn't find message in channel quest.");
+          
+          var messageAtInMinutes = Math.round(message.createdTimestamp / parseInt(quest.message));
+          if (!chatInChannel[message.author.id.toString()]) chatInChannel[message.author.id.toString()] = [messageAtInSeconds];
+          else if (chatInChannel[message.author.id.toString()].isArray()){ // if quest not completed yet
+            var intervalExists = (chatInChannel[message.author.id.toString()]).find((t) => t === messageAtInMinutes);
+            if (!intervalExists) chatInChannel[message.author.id.toString()] = [...chatInChannel[message.author.id.toString()], messageAtInMinutes];
+          }
+        }
+  
+        // add author id to array here for passive points (every min)
+        var authorExists = chatAuthorIds.find((a) => a === message.author.id.toString());
+        if (authorExists) return;
+        return chatAuthorIds.push(message.author.id.toString());
+    }
+    
+  } catch (err) {
+    console.log(err.message);
   }
 });
 
@@ -195,7 +202,7 @@ client.login(process.env.DISCORDJS_BOT_TOKEN).then(() => {
           for (var a of chatAuthorIdsCopy) {
             var statIncrease = Math.floor(Math.random() * (5 - 1 + 1)) + 1; // Random number between 1 (inclusive) and 5 (inclusive)
             var user = await User.findOne({ discordId: a });
-            if (!user) user = await User.create({ discordId: a, points: statIncrease });
+            if (!user) user = await User.create({ discordId: a, points: statIncrease, coins: statIncrease });
             else {
               user.points = user.points + statIncrease;
               user.coins = user.coins + statIncrease;
@@ -217,7 +224,7 @@ client.login(process.env.DISCORDJS_BOT_TOKEN).then(() => {
           for (var key of Object.keys(streamAuthorIdsCopy)) {
             if (streamAuthorIdsCopy[key] === true) {
               var user = await User.findOne({ discordId: key });
-              if (!user) user = await User.create({ discordId: a, points: 100 });
+              if (!user) user = await User.create({ discordId: a, points: 100, coins: 100 });
               else {
                 user.points = user.points + 100;
                 user.coins = user.coins + 100;
---
+++ models/Passive.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Passive = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Passive', Passive);
